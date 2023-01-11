4254c41dc15b9ee3c288529d52963581e52f33ad
izzadnazri <121816972+izzadnazri@users.noreply.github.com>
2023-01-11 10:51:13 +0800

    Delete NoOf_Teachers.R

--- NoOf_Teachers.R
+++
@@ -1,8 +0,0 @@
-library("ggplot2")
-
-View(NoOf_Teachers)
-
-ggplot(data=NoOf_Teachers)+ 
-  geom_col(mapping = aes(x=Year, y=Total, fill=Educational_stage, width = 0.6))+
-  labs(title="No of Teachers in Malaysia", subtitle = "From 2000-2020",
-       caption="Datasets from Jabatan Perangkaan Malaysia (DOSM)")
