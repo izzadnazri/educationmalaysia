4e525d07b9fabbc583db8ef7e4f10f575b60a76a
izzadnazri <121816972+izzadnazri@users.noreply.github.com>
2023-01-11 10:51:42 +0800

    Delete SPM_Achievement.R

--- SPM_Achievement.R
+++
@@ -1,9 +0,0 @@
-library("ggplot2")
-
-SPM_Achievement <- clean_names(SPM_Achievement)
-colnames(SPM_Achievement)[1] ="Year"
-colnames(SPM_Achievement)[5] ="NumberOfCandidates"
-ggplot(subset(SPM_Achievement,grade %in% c("All Grade A(s)")))+ 
-  geom_col(mapping = aes(x=Year, y=NumberOfCandidates, width = 0.6),fill="orange")+
-  labs(title="SPM Achievement in Malaysia", subtitle = "All Grade A(s) from 2014-2020",
-       caption="Datasets from Jabatan Perangkaan Malaysia (DOSM)")
