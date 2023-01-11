85c9feae25b3bb6e36baba49bee178d1e186bed7
izzadnazri <121816972+izzadnazri@users.noreply.github.com>
2023-01-11 10:51:07 +0800

    Delete NoOf_Students.R

--- NoOf_Students.R
+++
@@ -1,8 +0,0 @@
-library("ggplot2")
-
-View(NoOf_Students)
-
-ggplot(data=NoOf_Students)+ 
-  geom_col(mapping = aes(x=Year, y=Total, fill=Educational_stage, width = 0.6))+
-  labs(title="No of Students in Malaysia", subtitle = "From 2000-2020",
-       caption="Datasets from Jabatan Perangkaan Malaysia (DOSM)")
